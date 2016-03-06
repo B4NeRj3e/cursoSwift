20ce71d77446da6fb35652d9b8bc2a0ebb025b12
Ardiel David Redondo Vilaplana <sir_darkelf@hotmail.com>
2016-03-06 20:59:20 +0100

    prueba

---
+++ Curso Swift/Curso Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Curso Swift
+//
+//  Created by Ardiel Redondo on 6/3/16.
+//  Copyright © 2016 Ardiel Redondo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
